76fb1a4a72c9f609e8ddac1fce686e92cdbc1ed2
Stephen Halwes <shalwes@pretalen.com>
2020-03-09 22:14:03 -0400

    printing out single lines now

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::UdpSocket;
+use std::io::{self, Write};
 
 fn main() -> std::io::Result<()> {
     loop {
@@ -6,12 +7,13 @@ fn main() -> std::io::Result<()> {
         let sock = UdpSocket::bind("0.0.0.0:1337").expect("Couldn't bind to address");
 
         let mut buf = [0];
-        let (size, _src) = sock.recv_from(&mut buf)
+        let size = sock.recv(&mut buf)
                               .expect("Didn't rx anything...");
 
         let c: char = buf[0] as char;
 
-        print!("{} ", c);
+        print!("{}", c);
+        io::stdout().flush();
 
         if c == '\0' {
             break;
