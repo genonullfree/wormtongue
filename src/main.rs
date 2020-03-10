f227ac45451a60c5a745cb9504e2c28d543fe3af
Stephen Halwes <shalwes@pretalen.com>
2020-03-09 22:27:04 -0400

    adding print_char function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,50 @@
 use std::net::UdpSocket;
 use std::io::{self, Write};
 
-fn main() -> std::io::Result<()> {
+static ASCIICODES: [&str;32] = [
+    "NUL",
+    "SOH",
+    "STX",
+    "ETX",
+    "EOT",
+    "ENQ",
+    "ACK",
+    "BEL",
+    "BS",
+    "HT",
+    "LF",
+    "VT",
+    "FF",
+    "CR",
+    "SO",
+    "SI",
+    "DLE",
+    "DC1",
+    "DC2",
+    "DC3",
+    "DC4",
+    "NAK",
+    "SYN",
+    "ETB",
+    "CAN",
+    "EM",
+    "SUB",
+    "ESC",
+    "FS",
+    "GS",
+    "RS",
+    "US"
+];
+
+fn print_char(uc: &u8) {
+
+        let c: char = *uc as char;
+
+        print!("{}", c);
+        io::stdout().flush();
+}
+
+fn main() {
     loop {
 
         let sock = UdpSocket::bind("0.0.0.0:1337").expect("Couldn't bind to address");
@@ -10,14 +53,12 @@ fn main() -> std::io::Result<()> {
         let size = sock.recv(&mut buf)
                               .expect("Didn't rx anything...");
 
-        let c: char = buf[0] as char;
-
-        print!("{}", c);
-        io::stdout().flush();
-
-        if c == '\0' {
-            break;
+        if size != 1 {
+            println!("Error, received more than 1 byte!");
+            continue;
         }
+
+        print_char(&buf[0]);
+
     };
-    Ok(())
 }
