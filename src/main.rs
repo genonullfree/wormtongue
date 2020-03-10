554799f7336ddb70a68bb01e9c4023344966ab15
Stephen Halwes <shalwes@pretalen.com>
2020-03-09 22:09:38 -0400

    rx 1 byte

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,19 @@ use std::net::UdpSocket;
 fn main() -> std::io::Result<()> {
     loop {
 
-    let mut sock = UdpSocket::bind("0.0.0.0:1337").expect("Couldn't bind to address");
+        let sock = UdpSocket::bind("0.0.0.0:1337").expect("Couldn't bind to address");
 
-    let mut buf = [0;32];
-    let (size, src) = sock.recv_from(&mut buf)
-                          .expect("Didn't rx anything...");
+        let mut buf = [0];
+        let (size, _src) = sock.recv_from(&mut buf)
+                              .expect("Didn't rx anything...");
 
-    let buf = &mut buf[..size];
+        let c: char = buf[0] as char;
 
-    for i in 0..size-1 {
-        print!("{:02x} ", buf[i]);
-    }
+        print!("{} ", c);
 
-    if buf[0] == 0 {
-        break;
-    }
+        if c == '\0' {
+            break;
+        }
     };
     Ok(())
 }
