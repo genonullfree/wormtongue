b126ab43fe0bd958e82ac4468ca083dd89b471ea
Stephen Halwes <shalwes@pretalen.com>
2020-03-09 21:59:44 -0400

    reworked main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::UdpSocket;
+
+fn main() -> std::io::Result<()> {
+    loop {
+
+    let mut sock = UdpSocket::bind("0.0.0.0:1337").expect("Couldn't bind to address");
+
+    let mut buf = [0;32];
+    let (size, src) = sock.recv_from(&mut buf)
+                          .expect("Didn't rx anything...");
+
+    let buf = &mut buf[..size];
+
+    for i in 0..size-1 {
+        print!("{:02x} ", buf[i]);
+    }
+
+    if buf[0] == 0 {
+        break;
+    }
+    };
+    Ok(())
 }
